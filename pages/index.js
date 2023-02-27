a819efd253235dca39cc0625d1d36ab86755314c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 15:48:04 +0300

    Deleted the contents part of the page

--- pages/index.js
+++ pages/index.js
@@ -28,18 +28,7 @@ export default function Home({ data })
         </nav>
       </header>
 
-      <main className={styles.main}>
-        {
-          data.map((ev) =>
-          {
-            return (<Link key={ev.id} href={`/events/${ev.id}`}>
-              <Image src={ev.image} alt={ev.title} width={100} height={100} />
-              <h1>{ev.title}</h1>
-              <p>{ev.description}</p>
-            </Link>)
-          })
-        }
-      </main>
+
 
       <footer className={styles.footer}>
         <p>@ {year} Some Dumbass Project</p>
