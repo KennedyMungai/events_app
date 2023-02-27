3e3b5d30e1f2feb5c15f2a35498fa7d47b7b136f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 15:45:54 +0300

    Copied the main contents from the pages index file

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const HomePage = () =>
 {
     return (
-        <div>HomePage</div>
+        <main className={styles.main}>
+            {
+                data.map((ev) =>
+                {
+                    return (<Link key={ev.id} href={`/events/${ev.id}`}>
+                        <Image src={ev.image} alt={ev.title} width={100} height={100} />
+                        <h1>{ev.title}</h1>
+                        <p>{ev.description}</p>
+                    </Link>)
+                })
+            }
+        </main>
     )
 }
 
