0dcbad857d4fbaca12b4b74cc8ae2afaad35b822
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 11:17:32 +0300

    Gave the links blank hrefs

--- pages/index.js
+++ pages/index.js
@@ -29,17 +29,17 @@ export default function Home()
       </header>
 
       <main className={styles.main}>
-        <Link href={ }>
+        <Link href='/'>
           <img src="" alt="" />
           <h2>Events In London</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure officiis labore tenetur rem magni eveniet velit dicta minus, ut, assumenda quae praesentium vel, nemo vero alias saepe unde. Quod sed quos eum reprehenderit error voluptatum tempora odio suscipit officia facilis numquam repellat, libero, velit ratione eligendi nisi magni esse?</p>
         </Link>
-        <Link href={ }>
+        <Link href='/'>
           <img src="" alt="" />
           <h2>Events In LA</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure officiis labore tenetur rem magni eveniet velit dicta minus, ut, assumenda quae praesentium vel, nemo vero alias saepe unde. Quod sed quos eum reprehenderit error voluptatum tempora odio suscipit officia facilis numquam repellat, libero, velit ratione eligendi nisi magni esse?</p>
         </Link>
-        <Link href={ }>
+        <Link href='/'>
           <img src="" alt="" />
           <h2>Events In Lima</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure officiis labore tenetur rem magni eveniet velit dicta minus, ut, assumenda quae praesentium vel, nemo vero alias saepe unde. Quod sed quos eum reprehenderit error voluptatum tempora odio suscipit officia facilis numquam repellat, libero, velit ratione eligendi nisi magni esse?</p>
