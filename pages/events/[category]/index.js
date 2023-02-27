9cd61ec05376453c2f80c89601baaeffb38f833b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 12:02:38 +0300

    Renamed it the EventsCategoryPage

--- pages/events/[category]/index.js
+++ pages/events/[category]/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-const EventsPerCity = () =>
+const EventsCategoryPage = () =>
 {
     return (
         <div>
@@ -16,4 +16,4 @@ const EventsPerCity = () =>
     )
 }
 
-export default EventsPerCity
+export default EventsCategoryPage
