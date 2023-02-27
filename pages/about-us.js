ae599269566dc50cba1e5d04114ddca3ab407e82
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 11:19:18 +0300

    Added some placeholder text

--- pages/about-us.js
+++ pages/about-us.js
@@ -5,6 +5,9 @@ const AboutUs = () =>
     return (
         <div>
             <h1>About Us Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi tempora dolore odit cupiditate ad non! Ratione nam adipisci minus animi quibusdam, sapiente harum debitis illum fugiat deleniti voluptas blanditiis doloribus.
+            </p>
         </div>
     )
 }
