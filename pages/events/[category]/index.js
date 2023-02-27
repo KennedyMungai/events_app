02251d82f42f4f5ccdd53729cec483290668b47f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 11:45:33 +0300

    Updated the Links

--- pages/events/[category]/index.js
+++ pages/events/[category]/index.js
@@ -6,12 +6,12 @@ const Events = () =>
     return (
         <div>
             <h1>Events In London</h1>
-            <Link href='/event/event1'>Event 1</Link>
-            <Link href='/event/event2'>Event 2</Link>
-            <Link href='/event/event3'>Event 3</Link>
-            <Link href='/event/event4'>Event 4</Link>
-            <Link href='/event/event5'>Event 5</Link>
-            <Link href='/event/event6'>Event 6</Link>
+            <Link href='/events/event1'>Event 1</Link>
+            <Link href='/events/event2'>Event 2</Link>
+            <Link href='/events/event3'>Event 3</Link>
+            <Link href='/events/event4'>Event 4</Link>
+            <Link href='/events/event5'>Event 5</Link>
+            <Link href='/events/event6'>Event 6</Link>
         </div>
     )
 }
