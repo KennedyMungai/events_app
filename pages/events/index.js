ff2e3ca07a9fbced0b0043795e8d4b7adfcc414c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 11:28:30 +0300

    Got rid of the descriptions

--- pages/events/index.js
+++ pages/events/index.js
@@ -10,38 +10,14 @@ const Events = () =>
             <Link href="/">
                 <Image src="" alt="" />
                 <h2>Events In London</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure
-                    officiis labore tenetur rem magni eveniet velit dicta minus, ut,
-                    assumenda quae praesentium vel, nemo vero alias saepe unde. Quod sed
-                    quos eum reprehenderit error voluptatum tempora odio suscipit
-                    officia facilis numquam repellat, libero, velit ratione eligendi
-                    nisi magni esse?
-                </p>
             </Link>
             <Link href="/">
                 <Image src="" alt="" />
                 <h2>Events In LA</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure
-                    officiis labore tenetur rem magni eveniet velit dicta minus, ut,
-                    assumenda quae praesentium vel, nemo vero alias saepe unde. Quod sed
-                    quos eum reprehenderit error voluptatum tempora odio suscipit
-                    officia facilis numquam repellat, libero, velit ratione eligendi
-                    nisi magni esse?
-                </p>
             </Link>
             <Link href="/">
                 <Image src="" alt="" />
                 <h2>Events In Lima</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A iure
-                    officiis labore tenetur rem magni eveniet velit dicta minus, ut,
-                    assumenda quae praesentium vel, nemo vero alias saepe unde. Quod sed
-                    quos eum reprehenderit error voluptatum tempora odio suscipit
-                    officia facilis numquam repellat, libero, velit ratione eligendi
-                    nisi magni esse?
-                </p>
             </Link>
         </div>
     )
