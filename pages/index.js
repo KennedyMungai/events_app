ded794df7ac5e88b3d782df83d07984fdabdcaae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 11:02:26 +0300

    Added a header tag with a child nav tag whose child is the img tag

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,13 @@ export default function Home()
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <header>
+        <nav>
+          <img src="" alt="" />
+        </nav>
+      </header>
+
       <main className={styles.main}>
 
       </main>
