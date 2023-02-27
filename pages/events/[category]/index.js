0d2adcc9c5954198dd5f138c5c19c766698fc8ee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 11:42:27 +0300

    Created a bunch of links

--- pages/events/[category]/index.js
+++ pages/events/[category]/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Events = () =>
@@ -5,6 +6,10 @@ const Events = () =>
     return (
         <div>
             <h1>Events In London</h1>
+            <Link href='/event/event1'></Link>
+            <Link href='/event/event2'></Link>
+            <Link href='/event/event3'></Link>
+            <Link href='/event/event4'></Link>
         </div>
     )
 }
