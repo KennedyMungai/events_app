312acc996f1edaecff18263b9dbaf5a70b2d1c9b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 13:06:46 +0300

    Added the key and the href for the Link tags

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,23 +2,23 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Events = ({ title }) =>
+const Events = ({ data }) =>
 {
     return (
         <div>
-            <h1>{title}</h1>
-            <Link href="/">
-                <Image src="" alt="" />
-                <h2>Events In London</h2>
-            </Link>
-            <Link href="/">
-                <Image src="" alt="" />
-                <h2>Events In LA</h2>
-            </Link>
-            <Link href="/">
-                <Image src="" alt="" />
-                <h2>Events In Lima</h2>
-            </Link>
+            <h1>Event Page</h1>
+            {data.map((event) =>
+            {
+                return (
+                    <Link
+                        href={`/events/${event.id}`}
+                        key={event.id}
+                    >
+                        <h2>{event.title}</h2>
+                        <p>{event.description}</p>
+                    </Link>
+                )
+            })}
         </div>
     )
 }
