a2da3118de9890746ba94cb53455f949afec6a22
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 12:47:21 +0300

    Uncommented the Image component and added the alt prop

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home({ data })
           data.map((ev) =>
           {
             return (<Link key={ev.id} href={`/events/${ev.id}`}>
-              {/* <Image src={ev.image} width={100} height={100} /> */}
+              <Image src={ev.image} alt={ev.title} width={100} height={100} />
               <h1>{ev.title}</h1>
               <p>{ev.description}</p>
             </Link>)
