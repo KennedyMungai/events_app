6abcf65d7b3a4c2b063b85264fb68bd4e95e0e47
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-27 15:52:50 +0300

    Added props to the component call

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({ data })
         </nav>
       </header>
 
-      <HomePageContent />
+      <HomePageContent data={data} />
 
       <footer className={styles.footer}>
         <p>@ {year} Some Dumbass Project</p>
