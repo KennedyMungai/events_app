81804ba76da1a8a01cc588d9c0242f16e83cea75
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-28 12:35:14 +0300

    Added the home-body class to the div that wraps the map

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -6,16 +6,18 @@ const HomePageContent = ({ data }) =>
 {
     return (
         <main>
-            {
-                data.map((ev) =>
+            <div className="home-body">
                 {
-                    return (<Link key={ev.id} href={`/events/${ev.id}`}>
-                        <Image src={ev.image} alt={ev.title} width={100} height={100} />
-                        <h1>{ev.title}</h1>
-                        <p>{ev.description}</p>
-                    </Link>)
-                })
-            }
+                    data.map((ev) =>
+                    {
+                        return (<Link key={ev.id} href={`/events/${ev.id}`}>
+                            <Image src={ev.image} alt={ev.title} width={100} height={100} />
+                            <h1>{ev.title}</h1>
+                            <p>{ev.description}</p>
+                        </Link>)
+                    })
+                }
+            </div>
         </main>
     )
 }
