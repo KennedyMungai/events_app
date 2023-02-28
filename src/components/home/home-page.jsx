accc8176d2af62ba0ec0b23dc402e7f4ef2e9623
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-28 12:39:12 +0300

    Changed the image sizes

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -10,11 +10,16 @@ const HomePageContent = ({ data }) =>
                 {
                     data.map((ev) =>
                     {
-                        return (<Link key={ev.id} href={`/events/${ev.id}`}>
-                            <Image src={ev.image} alt={ev.title} width={100} height={100} />
-                            <h1>{ev.title}</h1>
-                            <p>{ev.description}</p>
-                        </Link>)
+                        return (
+                            <Link
+                                key={ev.id}
+                                href={`/events/${ev.id}`}
+                                className='card'
+                            >
+                                <Image src={ev.image} alt={ev.title} width={300} height={300} />
+                                <h1>{ev.title}</h1>
+                                <p>{ev.description}</p>
+                            </Link>)
                     })
                 }
             </div>
