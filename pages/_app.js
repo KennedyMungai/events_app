ad3edc57c234243b11c43e42597dacb9cefa54cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-28 10:54:32 +0300

    Got rid of the header and footer components and their imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,8 @@
-import Footer from '@/src/components/footer/footer'
-import Header from '@/src/components/header/header'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps })
 {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </>
+    <Component {...pageProps} />
   )
 }
