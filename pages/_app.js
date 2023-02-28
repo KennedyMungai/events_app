0724c83b7304ddcd6225c6ef85ac46ef0bd3cfc2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-28 10:50:13 +0300

    Made sure header and footer are rendered out on every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import Footer from '@/src/components/footer/footer'
+import Header from '@/src/components/header/header'
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps })
+{
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
