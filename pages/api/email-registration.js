c24d5e4b0b097b904e6c6f3db05634772581cfd9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-28 15:57:32 +0300

    Added some logic inside the if block for the POST method

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -4,6 +4,12 @@ export default function handler(req, res)
 
     if (method === 'POST')
     {
+        const { email, eventId } = req.body
 
+        res.status(200).json(
+            {
+                message: `You have been registered successfully0`
+            }
+        )
     }
 }
