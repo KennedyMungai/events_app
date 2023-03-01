f0f65cfe8e2ffa76cd4c009945536e2359a2e905
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 10:04:33 +0300

    Stored the extracted data from the json file inside the data const

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -22,6 +22,7 @@ export default function handler(req, res)
     // Extract the data (Inside the allEvents object)
 
     const filePath = buildPath()
+    const data = extractData(filePath)
 
     if (method === 'POST')
     {
