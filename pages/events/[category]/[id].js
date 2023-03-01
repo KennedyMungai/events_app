43de8ac2ba55bdd52213d1d70621fddd65298f40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 11:06:11 +0300

    Created an if block to check the validity of the email address input

--- pages/events/[category]/[id].js
+++ pages/events/[category]/[id].js
@@ -16,6 +16,11 @@ const EventPage = ({ data }) =>
 
         const validRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
+        if (!emailValue.match(validRegex))
+        {
+
+        }
+
         try
         {
             // perform a fetch request for the api 
