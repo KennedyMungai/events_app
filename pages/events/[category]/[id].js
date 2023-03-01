df20ba79dc998c03d8621ee12b19614b32ae4d14
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 11:10:00 +0300

    Added a paragraph to render the error message of the mismatched regex

--- pages/events/[category]/[id].js
+++ pages/events/[category]/[id].js
@@ -77,6 +77,7 @@ const EventPage = ({ data }) =>
                     <button type="submit">Submit</button>
                 </div>
             </form>
+            <p>{message}</p>
         </div>
     )
 }
