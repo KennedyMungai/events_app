7d1ddb4149118bc35c0dc46709de16d6cc612c48
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 09:43:10 +0300

    Spaced out the response function calls

--- pages/api/email-registration.js
+++ pages/api/email-registration.js
@@ -6,10 +6,12 @@ export default function handler(req, res)
     {
         const { email, eventId } = req.body
 
-        res.status(200).json(
-            {
-                message: `You have been registered successfully with the email: ${email} ${eventId}`
-            }
-        )
+        res
+            .status(200)
+            .json(
+                {
+                    message: `You have been registered successfully with the email: ${email} ${eventId}`
+                }
+            )
     }
 }
