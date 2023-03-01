1f28633b6354a7d06536a752af835fbd3f9a09d4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-01 11:06:35 +0300

    Impirted the useState hook

--- pages/events/[category]/[id].js
+++ pages/events/[category]/[id].js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const EventPage = ({ data }) =>
 {
